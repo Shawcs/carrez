d5e065029c3d2fb80c43db7ef3d29a180527cfb4
shawcs <shawcs.t@gmail.com>
2016-02-08 18:12:50 +0100

    node server and scraping all together

---
+++ README.txt
@@ -0,0 +1,26 @@
+﻿
+
+INSTALATION:
+
+
+1. Ouvrez l'invite de commande et placer vous dans le fichier "./carez"
+
+2. Faites "npm install" 
+
+3. Puis "npm start" 
+
+4. Ouvrez votre navigateur et entrez "localhost:3000"
+
+
+
+Attention
+
+
+Le server web n'accepte pas les url mal formées ainsi que certaines annonces comportants
+des carractères spéciaux ainsi que des apostrophes dans les noms de villes.
+
+
+les url suivantes fonctionnent:
+un appartement : http://www.leboncoin.fr/ventes_immobilieres/903278558.htm?ca=12_s
+
+une maison : http://www.leboncoin.fr/ventes_immobilieres/845672483.htm?ca=12_s
---
+++ routes/server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var mAgents = require('./meilleurAgent.js');
+var boncoin = require('./boncoin.js');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static(__dirname +'../public'));
+
+/* GET home page. */
+
+router.get('/', function(req, res) {
+	res.render('contact');
+  //res.sendFile("index.html");
+});
+
+try{ //if the url is not good
+router.post('/',function (req, res)
+{	
+
+   boncoin(req.body.url, function (resultat) //name = nom de l'imput
+   {	
+    var prixAgent=0.0;
+	var Gdeal = "test";	
+	var Bdeal =" ";
+	
+	mAgents("/prix-immobilier/"+resultat.city+"-"+resultat.codeCity+"/#estimates" , function (result){
+
+		var typeBien = resultat.typeLogement;
+		var city = resultat.city;
+		var price = resultat.price;
+		var codeCity= resultat.codeCity;
+				
+		if(resultat.typeBien === "Ventes")
+			{
+				if(resultat.typeLogement === "Appartement")
+					{
+						prixAgent = parseFloat(result.priceFloat)*parseFloat(resultat.area);
+					}
+				else if(resultat.typeLogement ==="Maison")
+					{
+						prixAgent = parseFloat(result.priceHouse)*parseFloat(resultat.area);			
+					}	
+												
+			}
+			if(	resultat.typeBien === "Locations")
+			{
+				prixAgent = parseFloat(result.priceRent)*parseFloat(resultat.area);
+			}		
+			if ( prixAgent <= resultat.price)
+			{
+				console.log("BAD CHOICE");
+				Bdeal = "BAD CHOICE";
+				
+				res.render('contact',{city:city, Bdeal:Bdeal, price:price, typeBien:typeBien, prixAgent:prixAgent,codeCity:codeCity,image:image});
+			}
+			else
+			{
+				console.log("GOOD DEAL");
+				Gdeal = "GOOD DEAL ";
+				res.render('contact',{city:city, Gdeal:Gdeal, price:price, typeBien:typeBien, prixAgent:prixAgent,codeCity:codeCity,image:image});
+			}
+				
+		});
+		console.log(resultat);
+	});
+});
+	}
+		catch(err)
+		{ console.log(err);
+		 }
+
+
+module.exports = router;
